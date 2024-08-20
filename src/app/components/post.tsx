0070e26d81787a3d6ac9edca19a7c6f7482f7a2e
Juliano Warpechowski <julianogwarp@gmail.com>
2024-08-19 23:41:29 -0300

    responsiviness

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import useSWR from "swr";
 import { formatDistanceToNow } from "date-fns";
 
@@ -58,78 +58,76 @@ interface PostProps {
   onImageClick: (src: string) => void;
 }
 export default function Post({ onImageClick }: PostProps) {
- 
   const { data, error } = useSWR<DataProps>(
     `/api/data`,
     (url: string | URL | Request) => fetch(url).then((res) => res.json())
   );
-  console.log('data', data)
-if (error) return ;
-if (!data || !data.items)
-   return (
-     <ul className="px-24 drop-shadow-sm">
-       {Array.from({ length: 3 }).map((_, index) => (
-         <li
-           key={index}
-           className="bg-white my-4 flex border border-gray-200 rounded-md p-5 animate-pulse"
-         >
-           <div className="flex w-screen gap-x-4 justify-center">
-             <div className="flex border rounded-sm border-indigo-600 p-8 filter items-center">
-               <div className="object-cover max-w-32 max-h-32 bg-gray-200 h-32 w-32"></div>
-             </div>
-             <div className="min-w-0 flex-auto">
-               <div className="flex">
-                 <div className="flex-1">
-                   <div className="bg-gray-200 h-4 rounded-md mb-2 w-48"></div>
-                   <div className="bg-gray-200 h-4 rounded-md w-36"></div>
-                 </div>
-                 <div className="px-4 flex-1">
-                   <div className="bg-gray-200 h-4 rounded-md mb-2 w-32"></div>
-                   <div className="bg-gray-200 h-4 rounded-md w-24"></div>
-                 </div>
-               </div>
-               <div className="max-w-lg">
-                 <div className="bg-gray-200 h-4 rounded-md mb-2 w-full"></div>
-                 <div className="bg-gray-200 h-4 rounded-md w-3/4"></div>
-               </div>
-               <div>
-                 <div className="bg-gray-200 h-4 rounded-md mb-2 w-20"></div>
-                 <div className="bg-gray-200 h-4 rounded-md w-16"></div>
-               </div>
-             </div>
-             <div className="min-w-0 flex flex-col justify-between items-end">
-               <div className="bg-gray-200 h-4 w-52 rounded-md"></div>
-               <div className="bg-gray-200 h-4 rounded-md w-28 mt-4"></div>
-             </div>
-           </div>
-         </li>
-       ))}
-     </ul>
-   );
+  console.log("data", data);
+  if (error) return;
+  if (!data || !data.items)
+    return (
+      <ul className="px-4 drop-shadow-sm">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <li
+            key={index}
+            className="bg-white my-4 flex flex-col md:flex-row border border-gray-200 rounded-md p-5 animate-pulse"
+          >
+            <div className="flex w-full md:w-screen gap-x-4 justify-center">
+              <div className="flex border rounded-sm border-indigo-600 p-8 filter items-center">
+                <div className="object-cover max-w-32 max-h-32 bg-gray-200 h-32 w-32"></div>
+              </div>
+              <div className="min-w-0 flex-auto">
+                <div className="flex">
+                  <div className="flex-1">
+                    <div className="bg-gray-200 h-4 rounded-md mb-2 w-48"></div>
+                    <div className="bg-gray-200 h-4 rounded-md w-36"></div>
+                  </div>
+                  <div className="px-4 flex-1">
+                    <div className="bg-gray-200 h-4 rounded-md mb-2 w-32"></div>
+                    <div className="bg-gray-200 h-4 rounded-md w-24"></div>
+                  </div>
+                </div>
+                <div className="max-w-lg">
+                  <div className="bg-gray-200 h-4 rounded-md mb-2 w-full"></div>
+                  <div className="bg-gray-200 h-4 rounded-md w-3/4"></div>
+                </div>
+                <div>
+                  <div className="bg-gray-200 h-4 rounded-md mb-2 w-20"></div>
+                  <div className="bg-gray-200 h-4 rounded-md w-16"></div>
+                </div>
+              </div>
+              <div className="min-w-0 flex flex-col justify-between items-end">
+                <div className="bg-gray-200 h-4 w-52 rounded-md"></div>
+                <div className="bg-gray-200 h-4 rounded-md w-28 mt-4"></div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
 
   const { items, fetchedAt } = data;
   const relativeTime = formatDistanceToNow(new Date(fetchedAt), {
     addSuffix: true,
   });
-   const filteredItems = items.filter((item) =>
-     item.title.toLowerCase().includes("chico")
-   );
-   
-   
+  const filteredItems = items.filter((item) =>
+    item.title.toLowerCase().includes("chico")
+  );
+
   return (
-    <div className="px-24 drop-shadow-sm">
+    <div className="px-4 md:px-24 drop-shadow-sm">
       {filteredItems.length > 0 ? (
-        <ul className=" ">
+        <ul>
           {filteredItems.map((item, index: number) => (
             <li
-              className="bg-white my-4 flex border border-gray-200  rounded-md p-5"
+              className="bg-white my-4 flex flex-col md:flex-row border border-gray-200 rounded-md p-5"
               key={index}
             >
-              <div className="flex w-screen  gap-x-4 justify-center">
-                <div className="flex border  rounded-sm border-indigo-600 p-8 filter items-center ">
+              <div className="flex flex-col md:flex-row w-full gap-x-4 justify-center">
+                <div className="flex border rounded-sm border-indigo-600 p-8 justify-center filter items-center mb-4 md:mb-0">
                   {item.pagemap?.cse_image?.length > 0 && (
                     <img
-                      className="object-cover max-w-32 max-h-32 grayscale cursor-pointer hover:grayscale-0"
+                      className="object-cover max-w-full md:max-w-32 max-h-32 grayscale cursor-pointer hover:grayscale-0"
                       src={item.pagemap.cse_image[0].src}
                       alt={item.title}
                       width="200"
@@ -140,7 +138,7 @@ if (!data || !data.items)
                     />
                   )}
                 </div>
-                <div className="max-w-max flex-auto ">
+                <div className="max-w-full md:max-w-max flex-auto mb-4 md:mb-0">
                   {item.pagemap?.metatags?.length > 0 &&
                     !item.title.includes("chico") && (
                       <div className="flex">
@@ -155,7 +153,7 @@ if (!data || !data.items)
                       </div>
                     )}
                   {item.pagemap?.metatags?.length > 0 && (
-                    <div className="max-w-lg">
+                    <div className="max-w-full md:max-w-lg">
                       <p className="text-sm leading-6 text-gray-500">
                         Description
                       </p>
@@ -190,7 +188,7 @@ if (!data || !data.items)
                     </div>
                   )}
                 </div>
-                <div className="flex-auto pl-16">
+                <div className="flex-auto md:pl-16 mb-4 md:mb-0">
                   <p className="text-sm leading-6 text-gray-500">
                     Auction House
                   </p>
@@ -200,17 +198,17 @@ if (!data || !data.items)
                     ]?.toLowerCase()}
                   </p>
                 </div>
-                <div className="min-w-0 justify-center ">
+                <div className="min-w-0  justify-center md:justify-end">
                   <button
                     onClick={() =>
                       window.open(`${item.formattedUrl}`, "_blank")
                     }
-                    className="px-4 py-2 w-52 h-10 border rounded-md border-gray-200 text-black hover:bg-gray-100 focus:outline-none"
+                    className="px-4 py-2 w-full md:w-52 h-10 border rounded-md border-gray-200 text-black hover:bg-gray-100 focus:outline-none"
                   >
                     <p className="text-sm "> More Information</p>
                   </button>
-                  <p className=" flex justify-center text-gray-500 text-sm pt-2">
-                    Fetched in {relativeTime}
+                  <p className="flex justify-center text-gray-500 text-sm pt-2">
+                    Fetched {relativeTime}
                   </p>
                 </div>
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,59 @@
-
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import Post from "./components/post";
-const intervals = [
-  {
-    id: "1w",
-    name: "Every Week",
-    cron: "0 0 * * 0",
-  },
-];
+
 export default function Page() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
-   const handleCloseModal = () => {
-     setIsModalOpen(false);
-     setSelectedImage(null);
-   };
-   const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
-     if (event.target === event.currentTarget) {
-       handleCloseModal();
-     }
-   };
-   useEffect(() => {
-     const handleEscKey = (event: KeyboardEvent) => {
-       if (event.key === "Escape") {
-         handleCloseModal();
-       }
-     };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-     if (isModalOpen) {
-       document.addEventListener("keydown", handleEscKey);
-     } else {
-       document.removeEventListener("keydown", handleEscKey);
-     }
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
 
-     return () => {
-       document.removeEventListener("keydown", handleEscKey);
-     };
-   }, [isModalOpen]);
+  const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      handleCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        handleCloseModal();
+      }
+    };
+
+    if (isModalOpen) {
+      document.addEventListener("keydown", handleEscKey);
+    } else {
+      document.removeEventListener("keydown", handleEscKey);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [isModalOpen]);
+
   return (
     <>
-      <section className="pt-10 flex justify-center flex-col gap-2  px-24">
-        <h1 className="font-bold text-zinc-900 text-lg ">Chico da Silva / Auction Cron Job</h1>
+      <section className="pt-10 flex justify-center flex-col gap-2 px-4 md:px-24">
+        <h1 className="font-bold text-zinc-900 text-xl md:text-5xl">
+          Chico da Silva / Auction Cron Job
+        </h1>
         <div>
-          {" "}
-          <span className="font-light text-md  text-zinc-500">
+          <span className="font-light text-sm md:text-md text-zinc-500">
             Website created to display the last google search findings of the
-            brazillian artist Chico da Silva. The job runs every week.
+            Brazilian artist Chico da Silva. The job runs every week.
           </span>
         </div>
         <div>
-          {" "}
-          <span className="font-light text-md mb-4 text-zinc-500">
-            Created using next app router, vercel cron job, vercel kv, react and
-            tailwind.
+          <span className="font-light text-sm md:text-md mb-4 text-zinc-500">
+            Created using Next.js app router, Vercel cron job, Vercel KV, React,
+            and Tailwind CSS.
           </span>
         </div>
-        <hr ></hr>
+        <hr className="my-4" />
       </section>
       <div>
         <Post
@@ -71,7 +68,7 @@ export default function Page() {
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
           onClick={handleOutsideClick}
         >
-          <div className="relative bg-white p-4 rounded-lg shadow-lg">
+          <div className="relative bg-white p-4 rounded-lg shadow-lg max-w-xs md:max-w-md">
             <button
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
               onClick={handleCloseModal}
@@ -89,4 +86,3 @@ export default function Page() {
     </>
   );
 }
-
